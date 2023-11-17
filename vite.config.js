9e137b7a5056361c0874aedcfb31364111514396
luakitch <kitchel301@gmail.com>
2023-11-17 11:57:40 -0500

    typo fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio-v1/"
+  base: "/portfolio-v1/",
   plugins: [react()],
   css: {
     modules: {
